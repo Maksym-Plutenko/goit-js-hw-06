8c68f6a3831e5785968209e7af1ca9b58ea2c675
Maksym-Plutenko <plutenkom@gmail.com>
2022-12-08 22:29:18 +0200

    js 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const container = document.querySelector('#boxes');
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+createBtn.addEventListener('click', event => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  for (let index = 0; index < amount; index++) {
+    const box = document.createElement('div');
+    const boxSize = 30 + index*10;
+    const boxColor = getRandomHexColor();
+    box.style.display = 'block';
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = `${boxColor}`;
+    container.append(box);
+  }
+}
+
+function destroyBoxes() {
+  container.innerHTML = '';
 }
